79e57706d0d68ccd46e93204cf3d58115d6e17d0
sergio.pena <sergio.pena.orozco@gmail.com>
2025-08-22 23:58:03 -0600

    first commit

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    //
+    public function index() 
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        // Validaciones
+        $this->validate($request, [
+            'name' => 'required|max:30',
+            'username' => 'required|unique:users|min:3|max:30',
+            'email' => 'required|unique:users|email|max:60',
+            'password'=> 'required'
+        ]);
+    }
+}
