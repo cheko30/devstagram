5df6a71a2b864ae4806a955a9684af32f1b4cdcf
sergio.pena <sergio.pena.orozco@gmail.com>
2025-09-17 01:13:11 -0600

    add authentication users

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,9 +6,15 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+        
+    }
+
     public function index()
     {
-        dd('MURO');
+        return view('dashboard');
     }
 }
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -35,6 +35,15 @@ class RegisterController extends Controller
             'password' => Hash::make($request->password)
         ]);
 
+        // Autenticar usuario
+        // auth() ->attempt([
+        //     'email' => $request->email,
+        //     'password' => $request->password
+        // ]);
+
+        // Otra forma de autenticar
+        auth()->attempt($request->only('email', 'password'));
+
         // Redireccionar
         return redirect()->route('posts.index');
     }
